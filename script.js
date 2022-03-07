e2f80558d5c6feede0fc91793f1becdd20ad298a
Entropie67 <olivier.elophe@gmail.com>
2022-03-07 13:59:42 +0100

    premier clic

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ grille = document.querySelector("#jeu");
 
 
 for (let i=1; i<=9; i++){
-    grille.innerHTML += `<button id=\"case${i}\" class=\"case\" type=\"button\"> X </button>`;
+    grille.innerHTML += `<button id=\"case${i}\" class=\"case\" type=\"button\">  </button>`;
+
 }
+
+bouton1 = document.getElementById('case1');
+bouton1.addEventListener("click", (e) => {
+    alert("click");
+});
